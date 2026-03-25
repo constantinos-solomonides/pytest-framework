46830869ea24ee60902b02d2f8eacda98cce5a49
constantinos-solomonides <constantinos.st.solomonides@gmail.com>
2026-03-25 19:34:06 +0100

    Fix compose & `.env` generator files
    - Remove `retries` parameter from docker-compose
    - Use `realpath` and `dirname` instead of subshelled `cd` to get full
      path

--- scripts/generate_env.sh
+++ scripts/generate_env.sh
@@ -11,7 +11,7 @@
 
 set -euo pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 ENV_FILE="$PROJECT_ROOT/.env"
 FORCE=0
